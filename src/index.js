d940593874af2f1fb4171dac6106e463a601c6a6
zennarchi <art256100@gmail.com>
2019-01-29 22:30:57 +0200

    Basic react+redux structure Added actions for fetching data from fake api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
+import './main.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Resources/css/app.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {Router, Route} from 'react-router'
+
+import reducers from 'reducers';
+
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
 
 
-import { BrowserRouter } from 'react-router-dom';
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
-import Routes from './routes';
+const history = syncHistoryWithStore(browserHistory, store)
 
-const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes />
-        </BrowserRouter>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones}/>
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
